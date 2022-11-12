0b806de434dfe43da0afdd489d67162c496cafc5
ej <103414353+ej-uses-git@users.noreply.github.com>
2022-11-12 04:49:30 +0200

    Change to eslint

--- src/__tests__/removeFrom.test.ts
+++ src/__tests__/removeFrom.test.ts
@@ -1,4 +1,4 @@
-import remove from '../remove';
+import removeFrom from '../removeFrom';
 
 const ARRAY = [{ id: 1 }, { id: 2 }, { id: 3 }];
 const RESULT_ARRAY = [{ id: 1 }, { id: 3 }];
@@ -12,17 +12,17 @@ const RESULT_OBJECT = {
 };
 
 test('Remove an item from an array', () => {
-  expect(remove(ARRAY, (item: { id: number }) => item.id === 2)).toStrictEqual(
-    RESULT_ARRAY,
-  );
+  expect(
+    removeFrom(ARRAY, (item: { id: number }) => item.id === 2),
+  ).toStrictEqual(RESULT_ARRAY);
 });
 
 test('Remove a property from an object (by key)', () => {
-  expect(remove(OBJECT, 'lastName')).toStrictEqual(RESULT_OBJECT);
+  expect(removeFrom(OBJECT, 'lastName')).toStrictEqual(RESULT_OBJECT);
 });
 
 test('Remove an property from an object (by finder)', () => {
-  expect(remove(OBJECT, (value: string) => value === 'Shafran')).toStrictEqual(
-    RESULT_OBJECT,
-  );
+  expect(
+    removeFrom(OBJECT, (value: string) => value === 'Shafran'),
+  ).toStrictEqual(RESULT_OBJECT);
 });
